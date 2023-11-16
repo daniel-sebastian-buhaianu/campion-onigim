516934308fb2ba8b8edeaee9d06d1d64bb9678ca
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-11-16 14:44:55 +0200

    Update onigim.cpp - scor 100

--- onigim.cpp
+++ onigim.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <fstream>
 #define MAX_ELEVI 1000
 #define MAX_PUNCTAJ 300
@@ -16,66 +15,66 @@ struct ListaElevi
 };
 // variabile globale
 ListaElevi L[MAX_ELEVI+1];
-int nrElevi, nrPunctajeDistincte;
-int punctajDistinct[MAX_ELEVI], punctajElev[MAX_ELEVI];
-int aparitiiPunctaj[MAX_PUNCTAJ+1];
+int nrElevi, nrPunctajeDistincte, nrDistinctiiAcordate, nrMaxEleviCuAcelasiPunctaj;
+int punctajDistinct[MAX_ELEVI], punctajElev[MAX_ELEVI], aparitiiPunctaj[MAX_PUNCTAJ+1];
 // functii ajutatoare
-void initializeazaLista(ListaElevi & L);
-void adaugaInLista(ListaElevi & L, int id);
-void calculeazaPunctajeleElevilor();
+void citesteDateleDeIntrare();
+void calculeazaPunctajulObtinutDeFiecareElev();
+void calculeazaNumarulDeDistinctiiAcordateDeOrganizatori();
+void calculeazaNumarulMaximDeEleviCareAuObtinutAcelasiPunctaj();
+void afiseazaDateleDeIesire();
 // functia principala
 int main()
 {
-	int i;
-	ifstream fin("onigim.in");
-	fin >> nrElevi >> nrPunctajeDistincte;
-	for (i = 0; i < nrPunctajeDistincte; i++)
-	{
-		fin >> punctajDistinct[i];
-		initializeazaLista(L[i]);
-	}
-	initializeazaLista(L[nrPunctajeDistincte]);
-	for (i = 0; i < nrElevi; i++)
-	{
-		int nrEleviCuPunctajMaiMicDecatElevulI;
-		fin >> nrEleviCuPunctajMaiMicDecatElevulI;
-		adaugaInLista(L[nrEleviCuPunctajMaiMicDecatElevulI], i);
-	}
-	fin.close();
-	calculeazaPunctajeleElevilor();
-	ofstream fout("onigim.out");
-	for (i = 0; i < nrElevi; i++)
-		fout << punctajElev[i] << ' ';
-	int nrDistinctii = 0;
-	for (i = nrPunctajeDistincte-3; i < nrPunctajeDistincte; i++)
-		nrDistinctii += aparitiiPunctaj[punctajDistinct[i]];
-	fout << '\n' << nrDistinctii;
-	int nrMaxEleviCuAcelasiPunctaj = 0;
-	for (i = 0; i <= MAX_PUNCTAJ; i++)
-		if (aparitiiPunctaj[i] > nrMaxEleviCuAcelasiPunctaj)
-			nrMaxEleviCuAcelasiPunctaj = aparitiiPunctaj[i];
-	fout << '\n' << nrMaxEleviCuAcelasiPunctaj;	
-	fout.close();
+	citesteDateleDeIntrare();
+	calculeazaPunctajulObtinutDeFiecareElev();
+	calculeazaNumarulDeDistinctiiAcordateDeOrganizatori();
+	calculeazaNumarulMaximDeEleviCareAuObtinutAcelasiPunctaj();
+	afiseazaDateleDeIesire();
 	return 0;
 }
-void calculeazaPunctajeleElevilor()
+void afiseazaDateleDeIesire()
 {
-	for (int i = 0, j = 0; i < nrPunctajeDistincte && j <= nrPunctajeDistincte; i++)
-		if (L[j].nrElevi > 0)
-		{
-			for (Elev *e = L[j].primul; e; e = e->urmatorul)
-			{
-				int punctaj = punctajDistinct[i];
-				punctajElev[e->id] = punctaj;
-				aparitiiPunctaj[punctaj]++;
-			}
-			j += L[j].nrElevi;
-		}
+    ofstream fout("onigim.out");
+    for (int i = 0; i < nrElevi; i++)
+		fout << punctajElev[i] << ' ';
+    fout << '\n' << nrDistinctiiAcordate;
+    fout << '\n' << nrMaxEleviCuAcelasiPunctaj;
+    fout.close();
 }
-void initializeazaLista(ListaElevi & L)
+void calculeazaNumarulMaximDeEleviCareAuObtinutAcelasiPunctaj()
 {
-	L.nrElevi = 0;
-	L.primul = L.ultimul = 0;
+    for (int i = 0; i <= MAX_PUNCTAJ; i++)
+		if (aparitiiPunctaj[i] > nrMaxEleviCuAcelasiPunctaj)
+			nrMaxEleviCuAcelasiPunctaj = aparitiiPunctaj[i];
+}
+void calculeazaNumarulDeDistinctiiAcordateDeOrganizatori()
+{
+    int i;
+    if (nrPunctajeDistincte >= 3)
+        i = nrPunctajeDistincte-3;
+    else
+        i = 0;
+    while (i < nrPunctajeDistincte)
+    {
+        nrDistinctiiAcordate += aparitiiPunctaj[punctajDistinct[i]];
+        i++;
+    }
+}
+void calculeazaPunctajulObtinutDeFiecareElev()
+{
+    for (int i = 0, j = 0;
+         i < nrPunctajeDistincte && j < nrElevi; i++)
+            if (L[j].nrElevi > 0)
+            {
+                for (Elev *e = L[j].primul; e; e = e->urmatorul)
+                {
+                    int punctaj = punctajDistinct[i];
+                    punctajElev[e->id] = punctaj;
+                    aparitiiPunctaj[punctaj]++;
+                }
+                j += L[j].nrElevi;
+            }
 }
 void adaugaInLista(ListaElevi & L, int id)
 {
@@ -90,4 +89,26 @@ void adaugaInLista(ListaElevi & L, int id)
 		L.ultimul = noulElev;
 	}
 	L.nrElevi++;
+}
+void initializeazaLista(ListaElevi & L)
+{
+	L.nrElevi = 0;
+	L.primul = L.ultimul = 0;
+}
+void citesteDateleDeIntrare()
+{
+    int i;
+    ifstream fin("onigim.in");
+	fin >> nrElevi >> nrPunctajeDistincte;
+	for (i = 0; i < nrElevi; i++)
+        initializeazaLista(L[i]);
+	for (i = 0; i < nrPunctajeDistincte; i++)
+		fin >> punctajDistinct[i];
+	for (i = 0; i < nrElevi; i++)
+	{
+		int nrEleviCuPunctajMaiMicDecatElevulI;
+		fin >> nrEleviCuPunctajMaiMicDecatElevulI;
+		adaugaInLista(L[nrEleviCuPunctajMaiMicDecatElevulI], i);
+	}
+	fin.close();
 }
